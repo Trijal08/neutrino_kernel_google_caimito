1c5a1eaf18d59b2924ebf58d1c429707d977e6fd
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-09-13 12:48:16 -0400

    panel: caimito: km4: allow using different settings for sub-120Hz mode
    Co-Authored-By: Hamster Tian <6023221+updateing@users.noreply.github.com>

--- google-modules/display/panels/caimito/panel-gs-km4.c
+++ google-modules/display/panels/caimito/panel-gs-km4.c
@@ -259,8 +259,14 @@ module_param(use_segmented_dimming, int, 0644);
 u8 freq_cmd[4] = {0x00, 0x43, 0x43, 0x03};
 module_param_array(freq_cmd, byte, NULL, 0644);
 
+u8 freq_cmd_ns[4] = {0x00, 0x43, 0x43, 0x03};
+module_param_array(freq_cmd_ns, byte, NULL, 0644);
+
 u8 freq_cmd_high_brightness[4] = {0x00, 0x43, 0x43, 0x03};
 module_param_array(freq_cmd_high_brightness, byte, NULL, 0644);
+
+u8 freq_cmd_high_brightness_ns[4] = {0x00, 0x43, 0x43, 0x03};
+module_param_array(freq_cmd_high_brightness_ns, byte, NULL, 0644);
 
 static void km4_send_dimming_freq_cmd(struct gs_panel *ctx, int need_unlock, const u8 *cmd)
 {
@@ -301,12 +307,18 @@ static void km4_set_default_dimming(struct gs_panel *ctx, int need_unlock)
 static void km4_set_override_dimming(struct gs_panel *ctx, int need_unlock)
 {
 	struct km4_panel *spanel = to_spanel(ctx);
+	const struct gs_panel_mode *pmode = ctx->current_mode;
+	struct gs_panel_status *sw_status = &ctx->sw_status;
+	unsigned long *feat = sw_status->feat;
+	const int vrefresh = drm_mode_vrefresh(&pmode->mode);
+	bool is_ns_mode = test_bit(FEAT_OP_NS, feat);
+	bool is_sub120 = vrefresh < 120;
 	u8 *cmd;
 
 	if (use_segmented_dimming && spanel->requested_brightness > DIMMING_SWITCH_THRESHOLD) {
-		cmd = freq_cmd_high_brightness;
+		cmd = (is_ns_mode || is_sub120) ? freq_cmd_high_brightness_ns : freq_cmd_high_brightness;
 	} else {
-		cmd = freq_cmd;
+		cmd = (is_ns_mode || is_sub120) ? freq_cmd_ns : freq_cmd;
 	}
 
 	km4_send_dimming_freq_cmd(ctx, need_unlock, cmd);
